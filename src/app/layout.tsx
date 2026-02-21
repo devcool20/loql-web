367f55afda09cd98e14a7abf29709a518c96adab
devcool20 <devcool20@gmail.com>
2026-02-21 21:22:20 +0530

    preview logo change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,6 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/opengraph-image.png",
-        width: 1200,
-        height: 630,
         alt: "loql - Community Sharing",
       },
     ],
